cdcbb4b119274f20b3f9de5b373e21649af9ffeb
itdany <danijel.fon@gmail.com>
2019-02-23 17:10:47 +0100

    after ejecting for android

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp1e1dd6674115405e9576a3095ea73174://192.168.1.40:80";
+
+}
